89b187cc623bfb063a90e68dedde44bb31fb5811
Gabriel-developments <277074371+GabrielOliveiraDeveloper@users.noreply.github.com>
2026-07-21 20:41:53 -0300

    Update authcontrollers.js

--- server/controllers/authcontrollers.js
+++ server/controllers/authcontrollers.js
@@ -6,10 +6,9 @@ const jwt = require('jsonwebtoken');
 const bcrypt = require('bcryptjs');
 
 const sendWelcomeEmail = async (to, userName) => {
+    // Usar service: 'gmail' contorna os bloqueios de porta do Render
     let transporter = nodemailer.createTransport({
-        host: 'smtp.gmail.com',
-        port: 465,
-        secure: true,
+        service: 'gmail',
         auth: {
             user: process.env.EMAIL_USER || 'sendermailservice01@gmail.com',
             pass: process.env.EMAIL_PASS || 'slht vdcm pfgi mmru'
