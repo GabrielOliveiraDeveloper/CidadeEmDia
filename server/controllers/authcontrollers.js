1f564f330f18999f1d9a8b10f18f74db163e97a5
Gabriel-developments <277074371+GabrielOliveiraDeveloper@users.noreply.github.com>
2026-07-21 19:44:21 -0300

    Update authcontrollers.js

--- server/controllers/authcontrollers.js
+++ server/controllers/authcontrollers.js
@@ -8,8 +8,8 @@ const bcrypt = require('bcryptjs');
 const sendWelcomeEmail = async (to, userName) => {
     const transporter = nodemailer.createTransport({
         host: process.env.EMAIL_HOST,
-        port: 587,
-        secure: false,
+        port: 465,
+        secure: true,
         auth: {
             user: process.env.EMAIL_USER,
             pass: process.env.EMAIL_PASS,
